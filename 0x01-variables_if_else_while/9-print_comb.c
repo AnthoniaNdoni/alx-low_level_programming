2164933c814f586113d4268b6e81eefc36b6b9fc
AnthoniaNdoni <tamarandoni@gmail.com>
2023-03-18 02:35:06 +0000

    my C program

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *  * main - Entry point
+ *   * Description:'print the numbers separated by , '
+ *    * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10 ; i++)
+	{
+			putchar(48 + i);
+			if (i != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	}
+	putchar('\n');
+	return (0);
+}
