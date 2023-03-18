06d9823254164d0bf10cecec441c87f6aca8d7d4
AnthoniaNdoni <tamarandoni@gmail.com>
2023-03-18 04:04:28 +0000

    my C program

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *  * main - Entry point
+ *   * Description:'printing all possible different combinations of two two digits'
+ *    * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}	
