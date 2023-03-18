b391978af32f1446d056ac85ed85f61d53479b2c
AnthoniaNdoni <tamarandoni@gmail.com>
2023-03-18 02:01:39 +0000

    my C program

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,13 +6,13 @@
  *  * main - Entry point
  *   * Description:'print the numbers.'
  *    * Return: Always 0 (Success)
- *     */
+ **/
 int main(void)
 {
-		int i = 0;
+	int i = 0;
 
-			for (i = 0; i < 10 ; i++)
-						putchar(48 + i);
-				putchar('\n');
-					return (0);
+	for (i = 0; i < 10 ; i++)
+		putchar(48 + i);
+	putchar('\n');
+	return (0);
 }
