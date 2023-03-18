9db6ea79834913c42368539743a1d120dd99189f
AnthoniaNdoni <tamarandoni@gmail.com>
2023-03-18 17:59:43 +0000

    my C program

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,19 +3,31 @@
 
 /* betty style doc for function main goes there */
 /**
- *  * main - Entry point
- *   * Description:'printing all possible different combinations of two two digits'
- *    * Return: Always 0 (Success)
- **/
+ * main - Entry point
+ * Description:'printing all possible different combinations of two two digits'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i = 0;
+	int first, second;
 
-	while (i < 10)
+	for (first = 0; first <= 98; first++)
 	{
-		printf("%d", i);
-		i++;
+		for (second = first + 1; second <= 99; second++)
+		{
+			putchar(48 + (first / 10));
+			putchar(48 + (first % 10));
+			putchar(' ');
+			putchar(48 + (second / 10));
+			putchar(48 + (second % 10));
+			if (!(first == 98 && second == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-	printf("\n");
+	putchar('\n');
+
 	return (0);
-}	
+}
