54b9935aa93d303c8fec804ed9c139bc649a01d1
AnthoniaNdoni <tamarandoni@gmail.com>
2023-03-18 04:31:11 +0000

    my C program

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,13 +3,14 @@
 
 /* betty style doc for function main goes there */
 /**
- *  * main - Entry point
- *   * Description:'print the numbers.'
- *    * Return: Always 0 (Success)
- **/
+ *  main - Entry point
+ *  Description:'print the numbers.'
+ *  Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int i = 0;
+
 	while (i < 10)
 	{
 		printf("%d", i);
