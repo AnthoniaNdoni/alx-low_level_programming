21f946fe86eb376e8ab218d32c5254ab49f01272
AnthoniaNdoni <tamarandoni@gmail.com>
2023-03-18 19:33:36 +0000

    my C program

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,18 +12,31 @@ int main(void)
 	int i, j;
 
 	for (i = 0; i < 9 ; i++)
+
 	{
 		for (j = i + 1 ; j < 10 ; j++)
+
 		{
+
 			putchar(48 + i);
+
 			putchar(48 + j);
+
 			if (i != 8)
+
 			{
+
 				putchar(',');
+
 				putchar(' ');
+
 			}
+
 		}
+
 	}
+
 	putchar('\n');
+
 	return (0);
 }
