764d8480c50cc0040d89fe223a3b4104e45ce148
AnthoniaNdoni <tamarandoni@gmail.com>
2023-03-18 06:07:55 +0000

    my C program

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,26 +4,26 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
- * Description:'printing all possible different combinations of two digits. '
+ * Description:'print all possible different combinations of two digits. '
  * Return: Always 0 (Success)
 */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9 ; i++)
+	for (i = 0; i < 9 ; i++	)
 	{
 		for (j = i + 1 ; j < 10 ; j++)
 		{
 			putchar(48 + i);
 			putchar(48 + j);
-			if (i != 8)
-			{
+	        	if (i != 8)
+                        {
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-	putchar('\n');
+        putchar('\n');
 	return (0);
 }	
