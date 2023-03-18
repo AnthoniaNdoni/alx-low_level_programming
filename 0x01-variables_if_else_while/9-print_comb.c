21cf3ddf93d37a7f4cfeea5a53bf78e0de4ab0da
AnthoniaNdoni <tamarandoni@gmail.com>
2023-03-18 04:46:59 +0000

    my C program

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,12 @@ int main(void)
 
 	for (i = 0; i < 10 ; i++)
 	{
-			putchar(48 + i);
-			if (i != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		putchar(48 + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
