94b7590ad524d9bb2ebb33fd906b575205300b4d
AnthoniaNdoni <tamarandoni@gmail.com>
2023-03-23 02:08:39 +0000

    numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description:'printing the first 98 fibonacci number '
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int a = 1, b = 2, sum;
+	int i;
+
+	printf("1, 2");
+	for (i = 1; i < 49 ; i++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %ld", sum);
+	}
+	printf("\n");
+	return (0);
+}
