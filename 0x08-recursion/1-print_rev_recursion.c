41f2c817754df43088215d5a96eac8f3c23aa7c7
AnthoniaNoni <tamarandoni@gmail.com>
2023-06-07 23:57:52 +0000

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,13 +9,13 @@ void _print_rev_recursion(char *s)
 {
 
 	if (*s == '\0') {
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
 
 	{
-		putchar(*s);
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 		return;
 	}
 }
